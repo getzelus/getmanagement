cc3b498f2dff0e78ba67a20b1a63456c38348944
get <chamoloss@live.fr>
2021-08-06 16:09:16 +0200

    change settings

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,6 +1,5 @@
 
 //const serverGlobal = 'http://localhost:8000/';
-//const serverGlobal = 'http://185.224.139.125:8000/';
 const serverGlobal = 'https://getmanagement.herokuapp.com/';
 
 const settings = {
